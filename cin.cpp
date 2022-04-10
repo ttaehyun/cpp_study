60c8dbeadf05313e29dec5524957450b3ebff4d3
Yoo taehyun <rth0824@naver.com>
2022-04-10 11:29:34 +0900

    2022.04.10 cin, cout, string

---
+++ cin.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void) {
+	string name;
+	cout << "이름을 입력하세요 ";
+	cin >> name;
+	cout << name << "을 환영합니다" << endl;
+	return 0;
+}
