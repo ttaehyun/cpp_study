f46705199452128f67d2a5db959a0a9b4cea1140
Yoo taehyun <rth0824@naver.com>
2022-04-14 21:20:52 +0900

    2022.04.14 pointer, reference

---
+++ swap1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void swap(int x, int y);
+
+int main() {
+	int a = 100, b = 200;
+	cout << "swap() 호출 전: a = " << a << ", b = " << b << endl;
+	swap(a, b);
+	cout << "swap() 호출 후: a = " << a << ", b = " << b << endl;
+	return 0;
+}
+
+void swap(int x, int y) {
+	int tmp;
+	tmp = x;
+	x = y;
+	y = tmp;
+}
